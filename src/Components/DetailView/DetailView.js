cc8210c7d342e16ae03fc882b8a149bc772453f2
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-22 15:58:19 -0700

    detail home button navigates back home when clicked

--- src/Components/DetailView/DetailView.js
+++ src/Components/DetailView/DetailView.js
@@ -1,10 +1,11 @@
 import './DetailView.css'
+import { Link } from 'react-router-dom'
 
 const DetailView = () => {
     return(
         <div className='detail-container'>
             <h1>NY Times News</h1>
-                <button className="home-button">Home</button>
+                <Link to={'/'}><button className="home-button">Home</button></Link>
             <div className='article-container'>
                 <p>Heyy some stuff whooo</p>
             </div>
