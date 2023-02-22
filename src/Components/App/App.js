b3f689f6c805768c1cd3955a9bd004906bb56c25
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-21 20:12:46 -0700

    fetched data to study it, commented it out

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,23 @@
 import logo from '../../logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const getData = () => {
+  //   fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=hJ0FcCaJDYmhdMWUGZW8WtEDnv3AGhsy')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log("Data: ", data.results[0].abstract)
+  //     return
+  //   })
+  // }
+
+  // useEffect(() => {
+  //   console.log("ok boss")
+  //   getData()
+  // }, [])
+
   return (
     <div className="App">
       <header className="App-header">
