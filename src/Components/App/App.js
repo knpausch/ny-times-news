1af707487b493c724c5787e3b0b49931b22d82de
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-22 14:29:08 -0700

    setup homeView component with basic styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
-import logo from '../../logo.svg';
-import './App.css';
-import { Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import './App.css'
+import { Route, Routes, Switch } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import HomeView from '../HomeView/HomeView'
 
 function App() {
   // const getData = () => {
@@ -19,23 +19,10 @@ function App() {
   // }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {<HomeView/>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/HomeView/HomeView.js
@@ -0,0 +1,33 @@
+import './HomeView.css'
+
+const HomeView = () => {
+    return (
+        <div className='home-container'>
+            <h1>NY Times News</h1>
+            <section class="dropdown-and-go">
+                <select class="dropdown-menu" name="section-types">
+                    <option value="select section">Select section</option>
+                    <option value="arts">arts</option>
+                    <option value="automobiles">automobiles</option>
+                    <option value="books">books</option>
+                    <option value="business">business</option>
+                    <option value="fashion">fashion</option>
+                    <option value="food">food</option>
+                    <option value="health">health</option>
+                    <option value="home">home</option>
+                    <option value="movies">movies</option>
+                    <option value="science">science</option>
+                    <option value="sports">sports</option>
+                    <option value="technology">technology</option>
+                    <option value="travel">travel</option>
+                    <option value="world">world</option>
+                </select>
+                <button class="go-button">Go</button>
+            </section>
+            <div className='article-card-container'>
+            </div>
+        </div>
+    )
+}
+
+export default HomeView
