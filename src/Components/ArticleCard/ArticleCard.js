5e3b00a6d5ed03c5df875afe7ef4f0323b50b3e8
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-02-23 15:18:45 -0700

    removed semicolons and console logs, also made all quotes single quote for consistency

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -3,7 +3,6 @@ import newsPaper from '../../Images/newspaper.png'
 import { NavLink } from 'react-router-dom'
 
 const ArticleCard = ({ title, setArticle }) => {
-    // console.log("hey look right here: ", title)
     return (
         <NavLink className='wrapper' to={`/article/${title}`}><div className='newspaper-and-title-container' onClick={() => setArticle(title)}>
             <img src={newsPaper} className='newspaper-img' />
--- src/Components/HomeView/HomeView.js
+++ src/Components/HomeView/HomeView.js
@@ -3,7 +3,7 @@ import ArticleCard from '../ArticleCard/ArticleCard'
 import { useEffect, useState } from 'react'
 
 const HomeView = ({ newsData, setArticle }) => {
-    const [newsSection, setNewsSection] = useState("")
+    const [newsSection, setNewsSection] = useState('')
     const [listOfArticles, setListOfArticles] = useState([])
 
     useEffect(() => {
@@ -35,35 +35,35 @@ const HomeView = ({ newsData, setArticle }) => {
     return (
         <div className='home-container'>
             <h1>NY Times News</h1>
-            <section className="dropdown-and-go">
-                <select className="dropdown-menu" name="section-types" onChange={event => handleChange(event)}>
-                    <option value="select section">(Select section)</option>
-                    <option value="arts">arts</option>
-                    <option value="automobiles">automobiles</option>
-                    <option value="books">books</option>
-                    <option value="business">business</option>
-                    <option value="climate">climate</option>
-                    <option value="fashion">fashion</option>
-                    <option value="food">food</option>
-                    <option value="health">health</option>
-                    <option value="home">home</option>
-                    <option value="insider">insider</option>
-                    <option value="magazine">magazine</option>
-                    <option value="movies">movies</option>
-                    <option value="opinion">opinion</option>
-                    <option value="politics">politics</option>
-                    <option value="podcasts">podcasts</option>
-                    <option value="realestate">realestate</option>
-                    <option value="science">science</option>
-                    <option value="sports">sports</option>
-                    <option value="technology">technology</option>
-                    <option value="theater">theater</option>
-                    <option value="travel">travel</option>
-                    <option value="us">us</option>
-                    <option value="world">world</option>
+            <section className='dropdown-and-go'>
+                <select className='dropdown-menu' name='section-types' onChange={event => handleChange(event)}>
+                    <option value='select section'>(Select Section)</option>
+                    <option value='arts'>arts</option>
+                    <option value='automobiles'>automobiles</option>
+                    <option value='books'>books</option>
+                    <option value='business'>business</option>
+                    <option value='climate'>climate</option>
+                    <option value='fashion'>fashion</option>
+                    <option value='food'>food</option>
+                    <option value='health'>health</option>
+                    <option value='home'>home</option>
+                    <option value='insider'>insider</option>
+                    <option value='magazine'>magazine</option>
+                    <option value='movies'>movies</option>
+                    <option value='opinion'>opinion</option>
+                    <option value='politics'>politics</option>
+                    <option value='podcasts'>podcasts</option>
+                    <option value='realestate'>realestate</option>
+                    <option value='science'>science</option>
+                    <option value='sports'>sports</option>
+                    <option value='technology'>technology</option>
+                    <option value='theater'>theater</option>
+                    <option value='travel'>travel</option>
+                    <option value='us'>us</option>
+                    <option value='world'>world</option>
                 </select>
-                <button className="go-button" onClick={() => filterArticles()}>Go</button>
-                <button className="reset-button" onClick={() => resetArticles()}>Reset</button>
+                <button className='go-button' onClick={() => filterArticles()}>Go</button>
+                <button className='reset-button' onClick={() => resetArticles()}>Reset</button>
             </section>
             <div className='article-card-container'>
                 {articleCards}
